6f416e2d1ec5ba974619d6ebe7f3c0470082eb5b
Killian Courvoisier <dev.killian.courvoisier@gmail.com>
2021-05-12 14:05:39 +0200

    add redirect

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,13 @@
 // == Import npm
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 // == Import
 import './styles.scss';
 import Header from '../Header';
 import Posts from '../Posts';
 import Footer from '../Footer';
+import NotFound from '../NotFound';
 
 import categoriesData from '../../data/categories';
 import postsData from '../../data/posts';
@@ -22,13 +23,17 @@ const filterPosts = (category) => {
 const App = () => (
   <div className="blog">
     <Header list={categoriesData} />
-    {
-      categoriesData.map((categoryObject) => (
-        <Route exact key={categoryObject.route} path={categoryObject.route}>
-          <Posts list={filterPosts(categoryObject.label)} />
-        </Route>
-      ))
-    }
+    <Switch>
+      {
+        categoriesData.map((categoryObject) => (
+          <Route exact key={categoryObject.route} path={categoryObject.route}>
+            <Posts list={filterPosts(categoryObject.label)} />
+          </Route>
+        ))
+      }
+      <Redirect frome="/jquery" to="/react" />
+      <NotFound />
+    </Switch>
     <Footer year={2021} />
   </div>
 );
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './style.scss';
 
-const NotFound = () => {
-  <a href="/" className="not-found">404</a>;
-};
+const NotFound = () => (
+  <a href="/" className="not-found">404</a>
+);
 
 export default NotFound;
