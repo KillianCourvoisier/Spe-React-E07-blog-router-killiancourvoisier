7e7532ab743018e9b978022a87e7d5921072efe6
Killian Courvoisier <dev.killian.courvoisier@gmail.com>
2021-05-17 11:50:56 +0200

    add Spinner & toggler

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,6 +8,7 @@ import Header from '../Header';
 import Posts from '../Posts';
 import Footer from '../Footer';
 import NotFound from '../NotFound';
+import Spinner from '../Spinner';
 
 import categoriesData from '../../data/categories';
 import postsData from '../../data/posts';
@@ -21,35 +22,31 @@ const filterPosts = (category) => {
 
 // == Composant
 const App = () => {
-  const [state, setState] = useState({
-    count: 0,
-    prenom: 'BG',
-  });
+  const [loading, setLoading] = useState(false);
+  const toggle = () => setLoading(!loading);
   return (
     <div className="blog">
       <Header list={categoriesData} />
-      <p>Bienvenu {state.prenom} . T'as Master-click {state.count} fois sur le bouton</p>
-      <button
-        type="button"
-        onClick={() => {
-          setState({
-            ...state,
-            count: state.count + 1,
-          });
-        }}
-      >Cliquez-moi
-      </button>
-      <Switch>
-        {
+      <button type="button" onClick={toggle}>Toggle Loading</button>
+      {
+        loading && <Spinner />
+      }
+      {
+        !loading && (
+        <Switch>
+          {
         categoriesData.map((categoryObject) => (
           <Route exact key={categoryObject.route} path={categoryObject.route}>
             <Posts list={filterPosts(categoryObject.label)} />
           </Route>
         ))
       }
-        <Redirect frome="/jquery" to="/react" />
-        <NotFound />
-      </Switch>
+          <Redirect frome="/jquery" to="/react" />
+          <NotFound />
+        </Switch>
+        )
+      }
+
       <Footer year={2021} />
     </div>
   );
